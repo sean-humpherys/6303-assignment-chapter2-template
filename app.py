d5e096f4cb8791d28fba64f7d6ef2fc4b1f9172c
Sean Humpherys <shumpherys@wtamu.edu>
2020-09-02 23:16:27 -0500

    added testing code

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-#Chapter 2.1 Variables
+# Chapter 2.1 Variables
 
+import math
 students_count = 1000
 rating = 4.99
 is_published = True
 course_name = "Python Programming"
 print(students_count)
 
-#Chapter 2.2 Variable Names
+# Chapter 2.2 Variable Names
 
 students_count = 1000
 rating = 4.99
 is_published = True
 course_name = "Python Programming"
 
-#Chapter 2.3 Strings
+# Chapter 2.3 Strings
 
 course = "Python Programming"
 print(len(course))
@@ -24,7 +25,7 @@ print(course[0:])
 print(course[:3])
 print(course[:])
 
-#Chapter 2.4 Escape Sequences
+# Chapter 2.4 Escape Sequences
 # \"
 course = "Python \"Programming"
 print(course)
@@ -35,14 +36,14 @@ print(course)
 course = "Python \\Programming"
 print(course)
 
-#Chapter 2.5 Formatted Strings
+# Chapter 2.5 Formatted Strings
 
 first = "Sean"
 last = "Humpherys"
 full = f"{len(first)} {2 + 2}"
 print(full)
 
-#Chapter 2.6 String Methods
+# Chapter 2.6 String Methods
 
 course = " python programming"
 print(course.upper())
@@ -54,7 +55,7 @@ print(course.replace("p", "j"))
 print("pro" in course)
 print("swift" not in course)
 
-#Chapter 2.7 Numbers
+# Chapter 2.7 Numbers
 
 print(10 + 3)
 print(10 - 3)
@@ -68,16 +69,15 @@ x = 10
 x = x + 3
 x += 3
 
-#Chapter 2.8 Working with Numbers
+# Chapter 2.8 Working with Numbers
 
-import math
 
 print(round(2.9))
 print(abs(-2.0))
 
 print(math.ceil(2.2))
 
-#Chapter 2.9 Type Conversion
+# Chapter 2.9 Type Conversion
 
 x = input("x: ")
 y = int(x) + 1
@@ -87,3 +87,4 @@ print(f"x: {x}, y: {y}")
 # ""
 # 0
 # None
+print("testing_" * 10)
